fix : save lora 16 precision

lora_diffusion/lora.py
@@ -185,8 +185,8 @@ def save_lora_weight(
     for _up, _down in extract_lora_ups_down(
         model, target_replace_module=target_replace_module
     ):
-        weights.append(_up.weight)
-        weights.append(_down.weight)
+        weights.append(_up.weight.to("cpu").to(torch.float16))
+        weights.append(_down.weight.to("cpu").to(torch.float16))
 
     torch.save(weights, path)
 