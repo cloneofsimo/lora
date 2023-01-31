5d6eb511565ee31efa01e7e5af0ca445fc28599b
SimoRyu <cloneofsimo@korea.ac.kr>
2023-01-31 18:25:11 +0000

    better example script

--- training_scripts/use_face_conditioning_example.sh
+++ training_scripts/use_face_conditioning_example.sh
@@ -1,6 +1,6 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export INSTANCE_DIR="./data_example_small"
-export OUTPUT_DIR="./exps/output_example_enid_w_mask"
+export INSTANCE_DIR="./data/data_captioned"
+export OUTPUT_DIR="./exps/krk_captioned_scale8"
 
 lora_pti \
   --pretrained_model_name_or_path=$MODEL_NAME  \
@@ -9,23 +9,28 @@ lora_pti \
   --train_text_encoder \
   --resolution=512 \
   --train_batch_size=1 \
-  --gradient_accumulation_steps=1 \
-  --learning_rate_unet=3e-4 \
-  --learning_rate_text=3e-4 \
-  --learning_rate_ti=1e-3 \
+  --gradient_accumulation_steps=2 \
+  --gradient_checkpointing \
+  --scale_lr \
+  --learning_rate_unet=2e-4 \
+  --learning_rate_text=1e-6 \
+  --learning_rate_ti=5e-4 \
   --color_jitter \
-  --lr_scheduler="constant" \
-  --lr_warmup_steps=100 \
-  --placeholder_tokens="<s1>|<s2>|<s3>" \
-  --placeholder_token_at_data="<s>|<s1><s2><s3>"\
-  --initializer_tokens="girl|<rand>|<rand>" \
+  --lr_scheduler="linear" \
+  --lr_warmup_steps=0 \
+  --lr_scheduler_lora="constant" \
+  --lr_warmup_steps_lora=100 \
+  --placeholder_tokens="<s1>|<s2>" \
+  --placeholder_token_at_data="<krk>|<s1><s2>" \
   --save_steps=100 \
   --max_train_steps_ti=500 \
   --max_train_steps_tuning=1000 \
   --perform_inversion=True \
-  --use_template="object"\
-  --weight_decay_ti=0.1 \
-  --weight_decay_lora=0.001\
-  --continue_inversion_lr=1e-4\
-  --device="cuda:0"\
-  --use_face_segmentation_condition\
+  --clip_ti_decay \
+  --weight_decay_ti=0.000 \
+  --weight_decay_lora=0.000 \
+  --device="cuda:0" \
+  --lora_rank=8 \
+  --use_face_segmentation_condition \
+  --lora_dropout_p=0.1 \
+  --lora_scale=8.0 \
