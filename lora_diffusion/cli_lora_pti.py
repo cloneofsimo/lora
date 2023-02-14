aab969c45fa45c42eac8ed22b937c0e301e15db4
xander <xandersteenbruggex@gmail.com>
2023-02-14 11:16:27 -0800

    print most similar tokens during ti phase

--- lora_diffusion/cli_lora_pti.py
+++ lora_diffusion/cli_lora_pti.py
@@ -71,8 +71,8 @@ def sim_matrix(a, b, eps=1e-8):
     """
     added eps for numerical stability
     """
-    b_n = b.norm(dim=1)[:, None]
-    a_norm = a / torch.max(b_n, eps * torch.ones_like(b_n))
+    a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
+    a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
     b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
     sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
     return sim_mt
